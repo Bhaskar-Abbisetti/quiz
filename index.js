281847dd18673b2aece0da2b0d82b3fee8dc12be
Bhaskar-Abbisetti <147693404+Bhaskar-Abbisetti@users.noreply.github.com>
2024-04-03 20:40:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 const MONGODB_URI = 'mongodb+srv://Gnane:Abm13abm13@cluster0.t4jkwqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(MONGODB_URI).then(()=>console.log("connected successfully"))
 
+app.get('/',(req,res)=>{
+  res.send("Hai")
+})
+
 app.use('/teacher', teacherRoutes);
 app.use('/student', studentRoutes);
 
